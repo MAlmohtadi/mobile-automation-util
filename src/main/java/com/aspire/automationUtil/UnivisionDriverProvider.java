25077040eb7697aceca39b551de95738d11f1536
unknown <msalem@WN-MSALEM.aspire-infotech.local>
2016-01-06 14:41:22 +0200

    Latest changes

--- src/main/java/com/aspire/automationUtil/UnivisionDriverProvider.java
+++ src/main/java/com/aspire/automationUtil/UnivisionDriverProvider.java
@@ -1,17 +1,22 @@
 package com.aspire.automationUtil;
 
+import static java.util.Arrays.asList;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Hashtable;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-
-
-
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.UnreachableBrowserException;
+import org.stringtemplate.v4.compiler.STParser.list_return;
+
 import com.saucelabs.saucerest.SauceREST;
 
 import io.appium.java_client.AppiumDriver;
@@ -20,11 +25,22 @@ import io.appium.java_client.ios.IOSDriver;
 
 public class UnivisionDriverProvider {
 	private Hashtable<String, AppiumDriver> drivers = new Hashtable<String, AppiumDriver>();
+	private Hashtable<String, serverInfo> servers = new Hashtable<String, serverInfo>();
 	private Date date = new Date();
 	public static Hashtable<String, String> sessions = new Hashtable<String, String>();
+	private ArrayList<String> PortsList = new ArrayList<String>();
+	private ArrayList<String> udid = new ArrayList<String>();
+
 	public enum platform {
 		ANDROID, IOS
 	}
+
+	public class serverInfo {
+		public int serverPort;
+		public String deviceUUID;
+	}
+
+	public static String jobName = null;
 
 	public static platform getPlatform() {
 		boolean android = TargetPlatform.platformName.toUpperCase().equals(
@@ -34,6 +50,22 @@ public class UnivisionDriverProvider {
 
 	public AppiumDriver getCurrentDriver() {
 		String threadName = Thread.currentThread().getName();
+		if (PortsList.size() == 0) {
+			  if (EnvirommentManager.getInstance().getProperty("UseLocaleEmulators").contains("true")) {
+				  String Ports = EnvirommentManager.getInstance().getProperty("Ports");
+				  PortsList.addAll(asList(Ports.split(",")));
+			  }else{
+				  PortsList.add("4723");  
+			  }
+		}
+
+			
+			  if (udid.size() == 0) {
+				  String udids = EnvirommentManager.getInstance().getProperty("udid");
+				  udid.addAll(asList(udids.split(",")));
+			  }
+		
+
 		if (!drivers.containsKey(threadName)) {
 			try {
 				SetupDriver(threadName);
@@ -50,10 +82,10 @@ public class UnivisionDriverProvider {
 			}
 
 			try {
-				String sessionId = drivers.get(threadName).getSessionId().toString();
+				String sessionId = drivers.get(threadName).getSessionId()
+						.toString();
 				sessions.put(threadName, sessionId);
-				if (sessionId.isEmpty())
-				{
+				if (sessionId.isEmpty()) {
 					SetupDriver(threadName);
 				}
 			} catch (Exception e) {
@@ -68,7 +100,27 @@ public class UnivisionDriverProvider {
 		return drivers.get(threadName);
 	}
 
+	@SuppressWarnings("null")
 	public void SetupDriver(String threadName) throws IOException {
+		serverInfo currentServer = new serverInfo();
+		if (servers.get(threadName) != null) {
+			currentServer = servers.get(threadName) ;
+		} else {
+
+			serverInfo server = new serverInfo();
+			synchronized (udid) {
+				server.deviceUUID = udid.get(0);
+				udid.remove(0);
+
+			}
+			synchronized (PortsList) {
+				server.serverPort = Integer.parseInt(PortsList.get(0).trim());
+				PortsList.remove(0);
+			}
+			servers.put(threadName, server);
+			currentServer = server;
+			// servers.ad
+		}
 		AppiumDriver driver = null;
 		// Setup capabilities
 
@@ -92,10 +144,23 @@ public class UnivisionDriverProvider {
 			capabilities.setCapability("browserName", "");
 			capabilities.setCapability("commandTimeout", "600");
 			capabilities.setCapability("maxDuration", "10800");
+			capabilities.setCapability("nativeInstrumentsLib", true);
+			// capabilities.setCapability("autoAcceptAlerts",
+			// "$.delay(10000); $.acceptAlert();");
+			capabilities.setCapability("waitForAppScript", "$.delay(3000);");
+
+			// capabilities.setCapability("fullReset", "true");
+			// capabilities.setCapability("noReset", "true");
+			// capabilities.setCapability("appActivity",
+			// "com.univision.SplashActivity");
+			// capabilities.setCapability("appWaitActivity",
+			// "com.univision.SplashActivity");
+			// capabilities.setCapability("appPackage", "com.univision");
+			// capabilities.setCapability("appWaitPackage", "com.univision");
 
 			// Set job name on Sauce Labs
 			capabilities.setCapability("name", System.getProperty("user.name")
-					+ " - Deportes(And) - " + date);
+					+ " - " + jobName + "(And) - " + date);
 		}
 		String userDir = System.getProperty("user.dir");
 
@@ -121,20 +186,53 @@ public class UnivisionDriverProvider {
 			if (!TargetPlatform.runOnAmazon) {
 				capabilities.setCapability("app", appPath);
 			}
-			serverAddress = new URL("http://127.0.0.1:4723/wd/hub");
-			try {
-				if (getPlatform() == platform.ANDROID) {
-					driver = new AndroidDriver(serverAddress, capabilities);
-				} else {
-					driver = new IOSDriver(serverAddress, capabilities);
-				}
 
-			} catch (Exception e) {
-				e.printStackTrace();
+		
+				System.out.println(currentServer.serverPort + ":" + currentServer.deviceUUID);
+				serverAddress = new URL("http://127.0.0.1:" + currentServer.serverPort
+						+ "/wd/hub".toString());
+			
+
+				try {
+					if (getPlatform() == platform.ANDROID) {
+						 if (EnvirommentManager.getInstance().getProperty("UseLocaleEmulators").contains("true")) {
+						capabilities.setCapability("udid", currentServer.deviceUUID);
+						 }
+						driver = new AndroidDriver(serverAddress, capabilities);
+					} else {
+						driver = new IOSDriver(serverAddress, capabilities);
+
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			
+		}
+
+		if (getPlatform() == platform.IOS) {
+			Alert alert = driver.switchTo().alert();
+			boolean autoAcceptAlerts = true;
+			int AcceptAlertsCounter = 0;
+			int tryCounter = 0;
+
+			while (autoAcceptAlerts) {
+				try {
+					alert.accept();
+					AcceptAlertsCounter++;
+					if (AcceptAlertsCounter == 2) {
+						autoAcceptAlerts = false;
+					}
+				} catch (Exception e) {
+					tryCounter++;
+					if (tryCounter == 10) {
+						autoAcceptAlerts = false;
+					}
+				}
 			}
 		}
 
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(180, TimeUnit.SECONDS);
 		drivers.put(threadName, driver);
 		System.out.println("Driver thread name:-----" + threadName
 				+ "---- and session id---" + driver.getSessionId().toString());
@@ -156,6 +254,7 @@ public class UnivisionDriverProvider {
 		// get the driver name
 		AppiumDriver driver = drivers.get(ThreadName);
 		if (driver != null) {
+
 			driver.quit();
 			driver = null;
 			// drivers.put(ThreadName, driver);
