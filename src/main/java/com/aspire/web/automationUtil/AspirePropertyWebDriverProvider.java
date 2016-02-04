585bc56926fbf2b62a434d534cd8b3503a775830
Feras Arabiat <ferasarabiat@gmail.com>
2016-02-04 15:10:34 +0200

    Update git ignore, add AspirePropertyWebDriverProvider.java

---
+++ src/main/java/com/aspire/web/automationUtil/AspirePropertyWebDriverProvider.java
@@ -0,0 +1,18 @@
+package com.aspire.web.automationUtil;
+
+import org.jbehave.web.selenium.PropertyWebDriverProvider;
+import org.jbehave.web.selenium.PropertyWebDriverProvider.Browser;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+public class AspirePropertyWebDriverProvider extends PropertyWebDriverProvider {
+
+	@Override
+	protected FirefoxDriver createFirefoxDriver() {
+
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("dom.max_chrome_script_run_time", 0);
+		profile.setPreference("dom.max_script_run_time", 0);
+		return new FirefoxDriver(profile);
+	}
+}
