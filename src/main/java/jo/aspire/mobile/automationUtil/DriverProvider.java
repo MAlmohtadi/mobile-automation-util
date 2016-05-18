6aa54e8a88f8ec5a80187cd42c0eb89e1d6a941c
Mohannad Salah <msalem@aspire-infotech.net>
2016-05-18 16:12:34 +0300

    test 1

--- src/main/java/jo/aspire/mobile/automationUtil/DriverProvider.java
+++ src/main/java/jo/aspire/mobile/automationUtil/DriverProvider.java
@@ -171,9 +171,8 @@ public serverInfo getCurrentServerInfo(String threadName)
 			capabilities.setCapability("browserName", "");
 			capabilities.setCapability("commandTimeout", "600");
 			capabilities.setCapability("maxDuration", "10800");
-			//capabilities.setCapability("nativeInstrumentsLib", true);
-			//capabilities.setCapability("waitForAppScript", "$.delay(3000);");
-			capabilities.setCapability("waitForAppScript", true);
+			capabilities.setCapability("nativeInstrumentsLib", true);
+			capabilities.setCapability("waitForAppScript", "$.delay(8000);");
 			capabilities.setCapability("notificationsAuthorized", true);
 			
 			
