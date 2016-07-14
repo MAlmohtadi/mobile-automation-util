7ddb64f705c34dbd01fe2a827fe4c845e2bdb22e
Mohannad Salah <msalem@aspire-infotech.net>
2016-07-14 15:39:09 +0300

    stop reset app

--- src/main/java/jo/aspire/mobile/automationUtil/DriverProvider.java
+++ src/main/java/jo/aspire/mobile/automationUtil/DriverProvider.java
@@ -357,10 +357,10 @@ public serverInfo getCurrentServerInfo(String threadName)
 					}
 					drivers.put(ThreadName, driver);
 				}else{
-					driver.resetApp();
-					System.out.println("reset: "+driver.getRemoteAddress().getPort() + ":"
-							+ driver.getCapabilities().getCapability("udid"));
-
+//					driver.resetApp();
+//					System.out.println("reset: "+driver.getRemoteAddress().getPort() + ":"
+//							+ driver.getCapabilities().getCapability("udid"));
+//
 				}
 			}
 		}catch(Exception ex){
